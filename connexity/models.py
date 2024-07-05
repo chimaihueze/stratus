291014d90ec292900d575a5d271d38fe6abdbb7b
chimaihueze <drkingsleyc@gmail.com>
2024-07-05 19:13:35 +0100

    created and added CustomUserManager and User models

--- connexity/models.py
+++ connexity/models.py
@@ -1,3 +1,45 @@
+import uuid
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and return a user with an email and password
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and return a superuser with an email and password
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=120, null=False)
+    last_name = models.CharField(max_length=120, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=120, null=False)
+    phone = models.CharField(max_length=120)
