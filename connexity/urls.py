c4b825122f4072f18929c3cf5eea31fb5fe65ce6
chimaihueze <drkingsleyc@gmail.com>
2024-07-06 11:53:57 +0100

    added url patterns

---
+++ connexity/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
+from connexity.views import LoginView, RegisterView
+
+urlpatterns = [
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh-pair'),
+    path('register', RegisterView.as_view(), name='register')
+]
