6ec93cac010ec172af41d1e1c92d893a780ee88b
chimaihueze <drkingsleyc@gmail.com>
2024-07-06 15:49:55 +0100

    modified create method to create an organisation when a user registers an account

--- connexity/serializers.py
+++ connexity/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from connexity.models import User
+from organisation.models import Organisation
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,6 +18,11 @@ class RegisterUserSerializer(UserSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+
+        organisation_name = f"{user.first_name.title()}'s Organisation"
+        organisation = Organisation.objects.create(name=organisation_name)
+
+        user.organisations.add(organisation)
         return user
 
 
