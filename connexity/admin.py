e2d847acf579aced09b0ff0723a0a4519ff88a2d
chimaihueze <drkingsleyc@gmail.com>
2024-07-06 11:56:16 +0100

    added admin registration config

--- connexity/admin.py
+++ connexity/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from connexity.models import User
+
+
+class AdminUser(UserAdmin):
+    model = User
+
+    list_display = ('first_name', 'last_name', 'email', 'is_staff')
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    search_fields = ('first_name', 'last_name', 'email')
+
+    ordering = ('email',)
+
+
+admin.site.register(User, AdminUser)
