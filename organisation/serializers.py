05dff4aff486dd29d79da0b8deac90d9508897a2
chimaihueze <drkingsleyc@gmail.com>
2024-07-06 15:50:34 +0100

    created serializers py file and added OrganisationSerializer

---
+++ organisation/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from organisation.models import Organisation
+
+
+class OrganisationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organisation
+        fields = ['name', 'description']
+
+    def create(self, validated_data):
+        organisation = Organisation.objects.create(**validated_data)
+        return organisation
