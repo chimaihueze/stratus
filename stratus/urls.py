60ad97738525dc075fab1fdc8ae5f631967a420c
chimaihueze <drkingsleyc@gmail.com>
2024-07-06 15:58:24 +0100

    refactored 'connexity' app name to 'users'

--- stratus/urls.py
+++ stratus/urls.py
@@ -7,5 +7,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('connexity.urls'))
+    path('auth/', include('users.urls'))
 ]
