291934994c8ce6cd705c3ec6152f37116c7ba88c
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 13:54:51 +0100

    added pytest fixtures for api_client and user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
 
-# Create your tests here.
+import pytest
+from rest_framework.test import APIClient
+
+from users.models import User
+
+
+@pytest.fixture
+def api_client():
+    """
+    API Client
+    """
+    return APIClient()
+
+
+@pytest.fixture()
+def user(db):
+    """
+    User
+    """
+    return User.objects.create_user(
+        {
+            "email": "k012@gmail.com",
+            "password": "ihembosi",
+            "first_name": "uche",
+            "last_name": "okafor",
+            "phone": "08033668887"
+        }
+    )
