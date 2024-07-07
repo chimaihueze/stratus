7dd6eef148c5e65eba1627ffce58111c3f1ef20b
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 08:56:55 +0100

    modified ACCESS_TOKEN_LIFETIME

--- stratus/settings.py
+++ stratus/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
