59d149e21a2347830ac33d40676634b65c1fbcdb
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 21:13:27 +0100

    removed and moved CustomUserManager to a separate file

--- users/models.py
+++ users/models.py
@@ -1,41 +1,10 @@
 import uuid
 
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
 from organisation.models import Organisation
-
-
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user manager
-    """
-
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and return a user with an email and password
-        """
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and return a superuser with an email and password
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
+from users.custom_user_manager import CustomUserManager
 
 
 class User(AbstractUser):
