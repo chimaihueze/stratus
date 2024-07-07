59deeb24929a1f9adb510901e7b4048d51e23fe7
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 21:15:03 +0100

    modified path name

--- organisation/urls.py
+++ organisation/urls.py
@@ -5,6 +5,6 @@ from organisation.views import OrganisationView, GetOrganisationView, AddUserToO
 urlpatterns = [
     path('', OrganisationView.as_view(), name='user-organisations'),
     path('/<str:pk>', GetOrganisationView.as_view(), name='organisation'),
-    path('/<str:pk>/users', AddUserToOrganisationView.as_view(), name='organisation'),
+    path('/<str:pk>/users', AddUserToOrganisationView.as_view(), name='add-to-organisation'),
     path('', CreateOrganisationView.as_view(), name='create-organisation'),
 ]
