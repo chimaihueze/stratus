ea78bd7aed0a65b4b843123fe4634c36bfbaf469
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 08:56:16 +0100

    created urls py and added url paths for endpoints

---
+++ organisation/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from organisation.views import OrganisationView, GetOrganisationView, AddUserToOrganisationView, CreateOrganisationView
+
+urlpatterns = [
+    path('', OrganisationView.as_view(), name='user-organisations'),
+    path('/<str:pk>', GetOrganisationView.as_view(), name='organisation'),
+    path('/<str:pk>/users', AddUserToOrganisationView.as_view(), name='organisation'),
+    path('', CreateOrganisationView.as_view(), name='create-organisation'),
+]
