4ede86fc9be506e6d250e09007c8762d2e74cfe0
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 21:16:59 +0100

    refactored first_name to firstName

--- organisation/services.py
+++ organisation/services.py
@@ -5,7 +5,7 @@ def create_organisation(user):
     """
     method to create organisation
     """
-    organisation_name = f"{user.first_name.title()}'s Organisation"
+    organisation_name = f"{user.firstName.title()}'s Organisation"
     organisation = Organisation.objects.create(name=organisation_name)
 
     return user.organisations.add(organisation)
