a41ef0ca031f23ec7b5e8ff76bf35c5a382231a2
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 21:14:27 +0100

    modified User model to suit requirements

--- users/models.py
+++ users/models.py
@@ -9,15 +9,33 @@ from users.custom_user_manager import CustomUserManager
 
 class User(AbstractUser):
     userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=120, null=False)
-    last_name = models.CharField(max_length=120, null=False)
+    firstName = models.CharField(max_length=120, null=False)
+    lastName = models.CharField(max_length=120, null=False)
     email = models.EmailField(unique=True, null=False)
     password = models.CharField(max_length=120, null=False)
     phone = models.CharField(max_length=15, null=True)
     organisations = models.ManyToManyField(Organisation, related_name='users')
+
     username = None
 
+    @property
+    def first_name(self):
+        return self.firstName
+
+    @first_name.setter
+    def first_name(self, value):
+        self.firstName = value
+
+    @property
+    def last_name(self):
+        return self.lastName
+
+    @last_name.setter
+    def last_name(self, value):
+        self.lastName = value
+
     objects = CustomUserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
