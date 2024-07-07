c84cba9d8170432ff8277731b0ceb776ce8275af
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 08:54:59 +0100

    modified url paths for endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from users.views import LoginView, RegisterView
+from users.views import LoginView, RegisterView, UserView
 
 urlpatterns = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh-pair'),
-    path('register', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
+    path('auth/register', RegisterView.as_view(), name='register'),
+    path('auth/login', LoginView.as_view(), name='login'),
+
+    path('api/users/<str:pk>', UserView.as_view(), name='user-detail')
 ]
