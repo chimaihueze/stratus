d939144888d6c0d6a2e6ea478b5368a1ac2115e9
chimaihueze <drkingsleyc@gmail.com>
2024-07-07 23:36:33 +0100

    added tests users app

--- users/tests.py
+++ users/tests.py
@@ -2,8 +2,11 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 import pytest
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APIClient
 
+from organisation.models import Organisation
 from users.models import User
 
 
@@ -23,8 +26,8 @@ def user(db):
     return User.objects.create_user(
         email="k@gmail.com",
         password="1234",
-        first_name="Test",
-        last_name="User",
+        firstName="Test",
+        lastName="User",
         phone="08033668887"
     )
 
@@ -48,3 +51,72 @@ def test_token_generation(api_client, user):
 
     assert abs((expiration_datetime - expected_expiration_time).total_seconds()) < 10
     assert decoded_token['userId'] == str(user.userId)
+
+
+@pytest.mark.django_db
+def test_register_user_with_default_organisation(api_client):
+    url = reverse('register')
+    data = {
+        'firstName': 'John',
+        'lastName': 'Doe',
+        'email': 'john.doe@example.com',
+        'password': 'password123'
+    }
+
+    response = api_client.post(url, data, format='json')
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert 'accessToken' in response.data['data']
+    assert 'user' in response.data['data']
+    assert response.data['data']['user']['firstName'] == 'John'
+    assert response.data['data']['user']['lastName'] == 'Doe'
+    assert response.data['data']['user']['email'] == 'john.doe@example.com'
+    organisation_exists = Organisation.objects.filter(name="John's Organisation").exists()
+    assert organisation_exists
+
+
+@pytest.mark.django_db
+def test_register_user_duplicate_email(api_client, user):
+    url = reverse('register')
+    data = {
+        'firstName': 'Test',
+        'lastName': 'User',
+        'email': 'k@gmail.com',
+        'password': '1234'
+    }
+
+    response = api_client.post(url, data, format='json')
+    assert 'status' in response.data
+    assert response.data['status'] == 'Bad request'
+    assert 'message' in response.data
+    assert response.data['message'] == 'Registration unsuccessful'
+    assert 'statusCode' in response.data
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('data', [
+    {'lastName': 'Doe', 'email': 'john.doe@example.com', 'password': 'password123'},  # firstName missing
+    {'firstName': 'John', 'email': 'john.doe@example.com', 'password': 'password123'},  # lastName missing
+    {'firstName': 'John', 'lastName': 'Doe', 'password': 'password123'},  # email missing
+    {'firstName': 'John', 'lastName': 'Doe', 'email': 'john.doe@example.com'},  # password missing
+])
+def test_register_user_missing_required_fields(api_client, data):
+    url = reverse('register')
+
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.django_db
+def test_user_login_successful(api_client, user):
+    url = reverse('login')
+    data = {
+        'email': 'k@gmail.com',
+        'password': '1234'
+    }
+
+    response = api_client.post(url, data, format='json')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert 'accessToken' in response.data['data']
